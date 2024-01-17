62c305f26bef3eb689104fb5eb7f12be8f4f42fe
rudinawang <rudnawang07@gmail.com>
2024-01-18 00:08:08 +0545

    get method

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express"); // requiring express package
 const app = express(); // storing it in app, app vannay variable throughout use garxum
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world from Nepal </h1>");
+  res.send(
+    "<h1>Hello world from Nepal </h1><p>This is Paragraph.</p><h2>I'm a Software Engineer.</h2><p>Good Night</p><h1>Rudra Nahawang Pandey</h1><p>Hello Developers</p>"
+  );
 });
 
 app.get("/about", (req, res) => {
